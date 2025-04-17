1fdaefffe2293abef8f36fc8640eb999e92d739f
JVRicci <54457696+JVRicci@users.noreply.github.com>
2025-04-16 23:42:12 -0300

    feature/Pesquisa resolvida

--- src/components/PokemonList.jsx
+++ src/components/PokemonList.jsx
@@ -6,19 +6,15 @@ export default function PokemonList({pokemonList =''}) {
     // const [isLoading, setIsLoading] = useState(true)
     
     // if (isLoading) return <p className='text-center mt-8 '>Carregando Pokemons...</p>;
-    
-    if (!Array.isArray(pokemonList)) {
-        return <p className="text-center mt-8 text-red-500">Erro: Lista inválida</p>;
-    }
 
-    if (pokemonList.length === 0) {
+    if (!Array.isArray(pokemonList) || pokemonList.length === 0) {
     return <p className="text-center mt-8 text-gray-500">Nenhum Pokémon encontrado</p>;
     }
     
     return (
         <div className="flex justify-center px-4 ">
             
-            <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4'>
+            <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 place-items-center gap-4'>
                 {pokemonList.map((item, index) => (
                     <PokemonChar 
                         key={index}
--- src/lib/api.js
+++ src/lib/api.js
@@ -2,25 +2,44 @@
 import api from '@/lib/axios'
 
 export const pokemonListQuery = async (pokemonName = '') => {
-    try{
-        const response = await api.get(pokemonName == '' ?  `/pokemon/${pokemonName}` : '/pokemon')
-        const results = response.data.results
-        const promises = results.map (pokemon => api.get(`/pokemon/${pokemonName ? pokemonName : pokemon.name}`)) 
+    if (pokemonName.trim() === ''){
+        try{
+            const response = await api.get(pokemonName == '' ?  `/pokemon/${pokemonName}` : '/pokemon')
+            const results = response.data.results
+            const promises = results.map (pokemon => api.get(`/pokemon/${pokemonName ? pokemonName : pokemon.name}`)) 
 
-        const details = await Promise.all(promises)
+            const details = await Promise.all(promises)
 
-        console.log(results)
-
-        const pokemonData = details.map(res => ({
-            name: res.data.name,
-            image: res.data.sprites.front_default,
-            type: res.data.types.map(t => t.type.name)
-        }))
-        
-        return pokemonData
+            const pokemonData = details.map(res => ({
+                name: res.data.name,
+                image: res.data.sprites.front_default,
+                type: res.data.types.map(t => t.type.name)
+            }))
+            
+            return pokemonData
+        }
+        catch(err){
+            console.log("Erro na requisição:" ,err)
+        }
     }
-    catch(err){
-        console.log("Erro na requisição:" ,err)
+
+    else {
+        try{
+            const response = await api.get(`/pokemon/${pokemonName}`)
+            const results = response.data
+
+            const pokemonData = {
+
+                name: results.name,
+                image: results.sprites.front_default,
+                type: results.types.map(t => t.type.name)
+            }
+            
+            return [pokemonData]
+        }
+        catch(err){
+            return []
+        }
     }
 }
 
